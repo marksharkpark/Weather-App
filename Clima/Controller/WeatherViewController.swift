cf48ec17d82592e2e09f471d370ba28a3b137f39
marksharkpark <39388430+marksharkpark@users.noreply.github.com>
2021-07-25 12:22:53 -0700

    Implementing UITextFieldDelegate
    - Updating functionality for user input when drawing up the keyboard.
    - Actions taken when user finishes entering a city
    - Updating README.md

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate{
+// The Text Field Delegate allows us to manage editing and validation of a text in a text field object
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self // the text field can communicate with the view controller by setting the view controller as the delegate
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) // dismisses the keyboard
+        print(searchTextField.text!)
+    }
+    
+    // Asks the delegate if the text field should process the pressing of the return key/button
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true) // dismisses the keyboard
+        return true // allow the text field to be allowed to return
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        }
+        else {
+            textField.placeholder = "Type something Here!" // changes the transparency pre-set text field word
+            return false
+        }
+    }
+    
+    // Clearing text field text
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Use searchTextField.text to get the weather for that city
+        print(searchTextField.text!)
+        searchTextField.text = ""
+    }
+    
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Weather-App
  An iOS app that will display the weather for a specific location. 
+ This application uses Swift protocols, the Delegate Design pattern, closures, extensions, and computed properites. 
+ I also used APIs to make HTTP requests with URLSession. 
+ To parse the JSON, I used the native JSONDecoder.
+ Retrieving GPS data by using Core Location. 
